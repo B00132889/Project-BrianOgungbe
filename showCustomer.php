3eee13eb032d2c0103fda08ea6951df90d4a9f2a
Brian Ogungbe <99332476+B00132889@users.noreply.github.com>
2022-12-10 01:04:45 +0000

    Updated line 6 to Select * from customers

--- showCustomer.php
+++ showCustomer.php
@@ -1,14 +1,9 @@
 <?php
-$fname = $_POST['fname'];
-$sname = $_POST['sname'];
-$email = $_POST['email'];
-$ph = $_POST['ph'];
-$dob = $_POST['dob'];
+
 
 include("dbcon.php");
 
-$sql = "insert into customers(firstname,surname,email,phone,dateofbirth)
-values ('$fname','$sname','$email','$ph','$dob')";
+$sql = "select * from customers";
    
 
 mysqli_query($con,$sql);
