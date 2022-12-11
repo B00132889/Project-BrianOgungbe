2906b83c1ec8edd06500655a2cb4e830b5799b88
Brian Ogungbe <99332476+B00132889@users.noreply.github.com>
2022-12-11 02:25:41 +0000

    Change showCustomer File to showCustomerForStaffID.php

---
+++ showCustomerForStaffID.php
@@ -0,0 +1,29 @@
+<?php
+
+
+include("dbcon.php");
+
+$sql = "select * from customers";
+   
+
+$result = mysqli_query($con,$sql);
+echo "<TABLE border='1'>";
+while($row=mysqli_fetch_assoc($result)) {
+	$fn = $row['firstname'];
+	$sn = $row['surname'];
+	$email = $row['email'];
+	$ph = $row['phone'];
+	$dob = $row['dateofbirth'];
+	
+	echo "<TR><TD>$fn</TD><TD>$sn</TD><TD>$email</TD><TD>$ph</TD><TD>$dob</TD></TR>";
+}
+echo "</TABLE>";
+
+
+
+
+mysqli_close($con);
+
+
+
+?>
